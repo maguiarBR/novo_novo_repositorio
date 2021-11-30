046b510511b360a768fdc1bc243c756fac43ec02
unknown <mviana.aguiar@gmail.com>
2021-11-30 19:43:07 -0300

    Tutorial, começo

--- Primeira versao.R
+++ Primeira versao.R
@@ -1,2 +1,19 @@
-# versão 1
-# versão 2
+# Tutorial para enviar as coisas para o github
+
+# primeiro, usamos o "git bash here" na pasta que queremos conectar ao github
+# depois precisamos adicionar os arquivos usando "git add "nome do arquivo""
+# precisamos também usar "git commit -m "mensagem" <- texto que vai ter na versão
+# enviada. Depois disso temos que "setar" o caminho que será enviado, ou seja,
+# realmente colocar o caminho para os arquivos serem enviados usando
+# git remote add origin "caminho .git" para poder "setar" o caminho que será usado
+# para criar o caminho válido para os arquivos serem enviados para o repositório
+# depois disso finalmente usamos o comando "git push -u origin master" para enviar
+# o arquivo que foi adicionado com A MENSAGEM QUE COMMITAMOS e o caminho que setamos
+# a ordem é sempre essa, adicionamos o arquivo usando add, fazemos o commit com a mensagem
+# usando o commit setamos o caminho que será enviado usando o remote, fazemos o envio
+# usando o push, dessa forma criamos a conexão mais básica usando o git no computador
+# existe também a forma de fazer "de forma automática" usando diretamente o RStudio
+
+# Tutorial para como criar branches (ramificações) do projeto
+
+# ótimo para criar partes nos projetos
